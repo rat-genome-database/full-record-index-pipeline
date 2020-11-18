dca4958a71835b62eb4f6e9ab3e9e0f9aeb0b936
tutajm <mtutaj@mcw.edu>
2020-11-18 16:26:47 -0600

    improved logging

--- src/main/dist/index.sh
+++ src/main/dist/index.sh
@@ -1,5 +1,15 @@
 # run fullRecordIndex pipeline with commandline parameters
 #    ("$@" passes all cmdline parameters to pipeline program)
 #
+. /etc/profile
+SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 APPHOME=/home/rgddata/pipelines/fullRecordIndex
+
+EMAIL_LIST=mtutaj@mcw.edu
+if [ "$SERVER" == "REED" ]; then
+  EMAIL_LIST=rgd.devops@mcw.edu
+fi
+
 $APPHOME/run.sh "$@"
+
+mailx -s "[$SERVER] Output from full record index pipeline " $EMAIL_LIST < $APPHOME/logs/summary.log
